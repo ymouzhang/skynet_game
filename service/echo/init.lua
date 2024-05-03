23793a69a79f3403427b6f7e79e924a648edcd5e
aaron <zhangqiandemsn@outlook.com>
2024-05-03 16:16:52 +0800

    fix: 为 echo 启用多人广播

--- service/echo/init.lua
+++ service/echo/init.lua
@@ -1,21 +1,29 @@
 local skynet = require "skynet"
 local socket = require "skynet.socket"
 
+local clients = {}
+
 function connect(fd, addr)
     --启用连接
     print(fd.." connected addr:"..addr)
     socket.start(fd)
+    -- 将新的 fd 放入列表中，暂时不存储 client 的相关信息
+    clients[fd] = {}
     --消息处理
     while true do
         local readdata = socket.read(fd)
         --正常接收
         if readdata ~= nil then
             print(fd.." recv "..readdata)
-            socket.write(fd, readdata)
+            -- 广播
+            for i, _ in pairs(clients) do
+                socket.write(fd,readdata)
+            end
         --断开连接
         else
             print(fd.." close ")
             socket.close(fd)
+            clients[fd] = nil
         end
     end
 end
